bf9163eb1dabbbcbba5da5b59b3aed13c32a2b3d
kezia17-imup <keziabaruzz61@gmail.com>
2026-09-21 05:21:50 +0700

    Labwork 01 - Prelab

---
+++ src/lw01/prelab/Main.java
@@ -0,0 +1,36 @@
+package lw01.prelab;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("jobs.txt");
+        if (!file.exists()) {
+            file = new File("src/lw01/prelab/jobs.txt");
+        }
+
+        List<PrintJob> jobs = new ArrayList<>();
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNext()) {
+            String type = scanner.next();
+            String id = scanner.next();
+            int pages = scanner.nextInt();
+
+            if (type.equals("MONO")) {
+                jobs.add(new MonoPrint(id, pages));
+            } else if (type.equals("COLOUR")) {
+                jobs.add(new ColourPrint(id, pages));
+            }
+        }
+        scanner.close();
+
+        for (PrintJob job : jobs) {
+            System.out.println(job.summary());
+        }
+    }
+}
